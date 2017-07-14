d73eb3c497df626f555979af656fc31b4642acb0
Albert <albertromaortiz@gmail.com>
2017-07-14 09:55:43 +0200

    copia de .gitignore original con .vendor ignorado

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/test', 'HomeController@index')->name('test');
+
 
 Route::group(['prefix' => 'eunomia' , 'middleware' => 'auth' ], function () {
 
