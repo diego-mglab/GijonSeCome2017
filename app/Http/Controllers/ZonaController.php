a25acb79ef5c511ea2c1dd185fed83959a692ad7
diego-mglab <diego@mglab.es>
2017-07-19 11:51:56 +0200

    Método update de contents

---
+++ app/Http/Controllers/ZonaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Zona;
+use Illuminate\Http\Request;
+
+class ZonaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $zonas = Zona::all();
+        return view('eunomia.zonas.listado_zonas', compact('zonas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('eunomia.zonas.form_ins_zonas');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Zona  $zona
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Zona $zona)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Zona  $zona
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Zona $zona)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Zona  $zona
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Zona $zona)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Zona  $zona
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Zona $zona)
+    {
+        //
+    }
+}
